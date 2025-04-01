9cd3a7d7c0debc8fb061b4a2926c5171248c89f0
AJPadillo <ajpadillo@gmail.com>
2025-04-01 18:24:27 +0200

    agregado fetchlatestinvoices y componente a page dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,13 @@
-import { fetchRevenue } from "../lib/data"
+import { fetchLatestInvoices, fetchRevenue } from "../lib/data"
 import { Card } from '@/app/ui/dashboard/cards';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import RevenueChart from "../ui/dashboard/revenue-chart";
+import latestInvoices from "@/app/ui/dashboard/latest-invoices";
 
 export default async function Page() {
     const revenue = await fetchRevenue()
+    const latestInvoices = await fetchLatestInvoices()
 
     return (
         <main>
@@ -24,7 +26,7 @@ export default async function Page() {
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <RevenueChart revenue={revenue} />
-                {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+                <LatestInvoices latestInvoices={latestInvoices} />
             </div>
         </main>
     );
