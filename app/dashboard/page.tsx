c2a074287d948df312c93787149a1e7f6f2dedac
AJPadillo <ajpadillo@gmail.com>
2025-04-01 19:13:04 +0200

    suspense en renderizado de revenuechart y sustituido revenue como prop en revenue-chart

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,11 +3,12 @@ import { Card } from '@/app/ui/dashboard/cards';
 import LatestInvoices from '../ui/dashboard/latest-invoices';
 import { lusitana } from '../ui/fonts';
 import RevenueChart from "../ui/dashboard/revenue-chart";
+import { Suspense } from "react";
+import { RevenueChartSkeleton } from "../ui/skeletons";
 
 export default async function Page() {
-    const revenue = await fetchRevenue()
     const latestInvoices = await fetchLatestInvoices()
-   
+
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -24,7 +25,9 @@ export default async function Page() {
         /> */}
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                <RevenueChart revenue={revenue} />
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                </Suspense>
                 <LatestInvoices latestInvoices={latestInvoices} />
             </div>
         </main>
