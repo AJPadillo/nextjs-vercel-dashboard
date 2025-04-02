5c9cb66b2e39dbb9a5ae92db958188de5b86a7d6
AJPadillo <ajpadillo@gmail.com>
2025-04-02 13:31:28 +0200

    agregado hook usesearchparams para capturar valores de la url

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,18 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  const handleSearch = (term:string) => {
-    console.log(term);
+  const searchParams = useSearchParams()
+  const handleSearch = (term: string) => {
+    const params = new URLSearchParams(searchParams)
+    if (term) {
+      params.set('search', term);
+    } else {
+      params.delete('search');
+    }
+    console.log(params.toString());
     
   }
   return (
@@ -13,6 +21,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         Search
       </label>
       <input
+        onChange={(event) => handleSearch(event.target.value)}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
       />
