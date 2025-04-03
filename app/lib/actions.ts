a4b08aaff314147f1451f24d068171343bf29402
AJPadillo <ajpadillo@gmail.com>
2025-04-03 13:26:40 +0200

    creado actions en lib (funciones servidor) y agregado a create-form de invoices

---
+++ app/lib/actions.ts
@@ -0,0 +1,8 @@
+'use server'
+
+// Marcar que todas las funciones que se exportan son funciones de servidor
+// y no se pueden usar en el cliente. Esto es importante para evitar errores de compilación
+
+export async function createInvoice(formData: FormData) {
+    
+}
