74b64d447e241db19eda32282ea8ab2b63e787cb
AJPadillo <ajpadillo@gmail.com>
2025-04-03 13:34:53 +0200

    agregado paquete zod. importado a actions.ts y creado schema de z object

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,8 +1,24 @@
 'use server'
+// Marca que todas las funciones que se exportan son funciones de servidor
+// y no se ejecuta ni envian al cliente. Esto es importante para evitar errores de compilación
 
-// Marcar que todas las funciones que se exportan son funciones de servidor
-// y no se pueden usar en el cliente. Esto es importante para evitar errores de compilación
+import { date, z } from 'zod'
+import { Invoice } from './definitions'
+
+const CreateInvoiceFormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['paid', 'pending']),
+    date: z.string()
+})
+
+
 
 export async function createInvoice(formData: FormData) {
-    
+    const rawFormData = {
+        customerID: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    }
 }
